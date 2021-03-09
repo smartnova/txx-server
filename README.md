ab95e51721d4cf6a7e751a3eed4ac48d30ad8c90
Ken Wakita <wakita@is.titech.ac.jp>
2021-03-09 16:05:28 +0900

    update

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# txx-server
+# 某所のソフトウェア設定
 
-以下をサーバで実行して下さい．
-~~~
-# 作業用ディレクトリの作成
-tmpdir=$(mktemp -d /tmp/wakita-`date "+%Y-%m-%d-%Hh%Mm"`) && cd $tmpdir                                           
-
-# 設定データのダウンロード
-git clone https://github.com/smartnova/txx-server.git
-
-# 設定スクリプトの実行
-cd txx-server; sh update.sh
-
-# 作業用ディレクトリの消去
-cd /tmp && rm -rf $tmpdir
-~~~
+サーバで `sudo wakita` したあとでスクリプト (`update.sh`) の内容を実行して下さい．
--- update.sh
+++ update.sh
@@ -1,8 +1,20 @@
-cd txx-server.git                               
-                                                 
-sh venv.sh                                          # Python3 仮想環境の構築    
-sh js.sh                                            # JavaScript の収集
-mkdir -p ~wakita/data && cp -r data/* ~wakita/data  # データのコピー
+#!/bin/sh
 
-                           
-chown -R wakita ~wakita/data ~wakita/lib
+if [ $USER != "wakita" ]; then echo "please sudo wakita first, before you run this script"; fi
+
+if [ "$1" = test ]; then export KWHOME=/tmp; else export KWHOME=/home/wakita-lab/wakita; fi
+export DATA=$KWHOME/data
+export SHARE=$KWHOME/share
+export REPOSITORY="$SHARE/`date +server-%Y-%m-%d`"
+#echo $KWHOME; exit 0
+
+# 作業用ディレクトリを作成して，そこに移動
+mkdir -p $SHARE; cd $SHARE
+
+# 設定データを展開
+git clone https://github.com/smartnova/txx-server.git $REPOSITORY
+cd $REPOSITORY
+
+sh venv.sh                                          # Python3 仮想環境の構築
+sh js.sh                                            # JavaScript の収集
+mkdir -p $DATA && cp -r data/* $DATA                # データのコピー
