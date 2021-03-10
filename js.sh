421b05414e8fbdc79443eb57d45f1968163349fe
Ken Wakita <wakita@is.titech.ac.jp>
2021-03-10 10:00:24 +0900

    vue, bootstrap

--- js.sh
+++ js.sh
@@ -12,5 +12,6 @@ dl() {
 dl d3 https://github.com/d3/d3/releases/download/v6.5.0/d3.zip
 dl c3 https://github.com/c3js/c3/archive/v0.7.20.zip
 dl jQuery https://code.jquery.com/jquery-3.6.0.min.js
+dl bootstrap https://github.com/twbs/bootstrap/releases/download/v5.0.0-beta2/bootstrap-5.0.0-beta2-dist.zip
 dl underscore https://underscorejs.org/underscore-min.js
 dl fontawesome https://use.fontawesome.com/releases/v5.15.2/js/all.js
